f8acaaa5ce2ebd2346656bdc34633ff807eeedd7
Akshay Jain <55624472+akshay2554980@users.noreply.github.com>
2020-12-20 13:35:20 +0530

    Update main.go

--- main.go
+++ main.go
@@ -3,14 +3,14 @@ package main
 
 import (
 	"context"
-    "encoding/json"
-    "fmt"
-    "log"
+        "encoding/json"
+        "fmt"
+        "log"
 	"net/http"
 	"go.mongodb.org/mongo-driver/bson"
-    "go.mongodb.org/mongo-driver/mongo"
-    "go.mongodb.org/mongo-driver/mongo/options"
-)
+        "go.mongodb.org/mongo-driver/mongo"
+        "go.mongodb.org/mongo-driver/mongo/options"
+      )
 
 
 // for returing json format to client
